400f9a26117428e3f6f067f7f079308fd7631358
Abhishek <abhishek.konnayil@gmail.com>
2025-05-09 23:42:01 +0530

    initial commit

---
+++ property/views.py
@@ -0,0 +1,20 @@
+
+from .models import Property
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from .serializers import PropertiesListSerializer
+from django.http import JsonResponse
+
+
+# Create your views here.
+
+@api_view(['GET'])
+@authentication_classes([])
+@permission_classes([])
+def properties_list(request):
+    properties = Property.objects.all()
+    serializer = PropertiesListSerializer(properties, many=True)
+
+    return JsonResponse({
+        'data': serializer.data
+    })
+0
