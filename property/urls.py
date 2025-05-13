a1c8407d554f67ca10b24715554818a0684cd141
Abhishek <abhishek.konnayil@gmail.com>
2025-05-14 02:17:18 +0530

    added properties via frontend

--- property/urls.py
+++ property/urls.py
@@ -4,4 +4,5 @@ from .import views
 
 urlpatterns = [
     path("", views.properties_list, name='property_list'),
+    path('create/', views.create_property, name='create_property')
 ]
--- property/views.py
+++ property/views.py
@@ -1,9 +1,13 @@
 
 from .models import Property
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import authenticate
 from .serializers import PropertiesListSerializer
 from django.http import JsonResponse
 from django.urls import reverse
+from .forms import PropertyForm
+from rest_framework import status
 
 
 # Create your views here.
@@ -21,4 +25,23 @@ def properties_list(request):
     })
 
 
-0
+@api_view(['POST', 'FILES'])
+@permission_classes([])
+def create_property(request):
+    try:
+        form = PropertyForm(request.POST, request.FILES)
+        trollan
+        if form.is_valid():
+            property:Property = form.save(commit=False)
+            property.lanlord = request.user
+        
+            
+
+            return JsonResponse({'success': True})
+        else:
+            print('error', form.errors, form.non_field_errors)
+
+            return JsonResponse({'errors': form.errors.as_json()}, status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        print(e)
+        raise e
