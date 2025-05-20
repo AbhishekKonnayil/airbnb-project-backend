a710c643b7031fee92c01da5192d46fab82b9b2f
Abhishek <abhishek.konnayil@gmail.com>
2025-05-20 15:58:57 +0530

    favorite feature completed

--- property/views.py
+++ property/views.py
@@ -28,9 +28,13 @@ def properties_list(request):
 
     properties = Property.objects.all()
     favorites = []
+
+    is_favorites = request.GET.get('is_favorites', '')
     landlord_id = request.GET.get('landlord_id', "")
     if landlord_id:
         properties = properties.filter(landlord_id=landlord_id)
+    if is_favorites:
+        properties = properties.filter(favorited__in=[user])
 
     if user:
         favorites = list(properties.filter(
